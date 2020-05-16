51c9c376f7fc42d0487dda374c161e49c3c4cce3
Athul P <athul929@gmail.com>
2020-05-17 01:43:43 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+var testData = {
+    max: 20,
+    data: corona,
+  };
+  
+  var baseLayer = L.tileLayer(
+    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+      attribution: '...',
+      maxZoom: 18
+    }
+  );
+  
+  var cfg = {
+    // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+    // if scaleRadius is false it will be the constant radius used in pixels
+    "radius": 0.8,
+    "maxOpacity": .8,
+    // scales the radius based on map zoom
+    "scaleRadius": true,
+    // if set to false the heatmap uses the global maximum for colorization
+    // if activated: uses the data maximum within the current map boundaries
+    //   (there will always be a red spot with useLocalExtremas true)
+    "useLocalExtrema": false,
+    // which field name in your data represents the latitude - default "lat"
+    latField: 'lat',
+    // which field name in your data represents the longitude - default "lng"
+    lngField: 'lng',
+    // which field name in your data represents the data value - default "value"
+    valueField: 'count'
+  };
+  
+  
+  var heatmapLayer = new HeatmapOverlay(cfg);
+  
+  var map = new L.Map('map', {
+    center: new L.LatLng(23.6586, 83.3568),
+    zoom: 4,
+    layers: [baseLayer, heatmapLayer]
+  });
+  
+  heatmapLayer.setData(testData);
